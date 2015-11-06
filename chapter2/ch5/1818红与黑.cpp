5d110c09a8648296f6a8d48c321d7e38e5933626
dahai78 <626071473@qq.com>
2015-11-06 22:02:54 +0800

    红与黑

--- chapter2/ch5/1818红与黑.cpp
+++ chapter2/ch5/1818红与黑.cpp
@@ -1,61 +1,71 @@
-﻿//版权所有 戴海源
-
 #include <iostream>
 #include <queue>
 using namespace std;
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 char ditan[20][20];
-int W,H:
-int X,Y;
-  
+int dx[]={-1,1,0,0};
+int dy[]={0,0,-1,1};
 struct Point{
 	int x;
-	int y;
-	
+	int y;		
 };          
 
-void read(){
+int W,H;
+int work(int m,int n){
 	
+	int answer=0;	
 	queue<Point> Q;
-	  
-    for(int i=0 ;i<H; i++)
-       
-       for(int j=0; j<W; j++)
-          
-          {
-            cin >> ditan[i][j];
-            if( ditan[i][j] =='@')
-               {
-               	 X=i; Y=j;
-               }
-          }
-          
-}
+	Point p ={m,n};
+    Q.push(p);
+    ditan[m][n]='#';
 
-void work(int x,int y){
+	while( !Q.empty()) {
+		Point t = Q.front();
+		Q.pop();
+		answer++;
+		for(int i=0;i<4;i++)
+		{
+		    int newx = t.x + dx[i];
+		    int newy =t.y + dy[i];
+		    	
+			if	( newx >=0 && newx < W  && newy>=0 && newy <H && ditan[ newx ][ newy] == '.'  ) 
+			{
+				Point p = {newx,newy};
+				Q.push(p);
+				ditan[newx][newy] ='#';
+		    }			
+		}
+		
+	}
 	
-	Point p;
-	p.x=x;  p.y=y;
-	q.
+	return answer;
 	
 }
  
 
 int main(int argc, char** argv) {
 	
-	cin >> W >> H;
-	
-	while( !(W==0 && j==0))
+    int X,Y;
+            
+	while( true)
 	{
-		work(X, Y);
-		
-		
-		cin >> W >> H;
-	}
+	   cin >> W >> H;
+	   if( W==0 && H==0) break;
+	   
+	   for(int i=0; i<H; i++)
+        for(int j=0; j<W; j++)
+         {
+            cin >> ditan[i][j];
+            if( ditan[i][j] =='@')
+               {
+               	 X=i; Y=j;
+               }
+          }     
+		   
+		cout << work(X, Y) << endl;		
 	
-          
-          
+	}     
 	
 	return 0;
 }
+
