0134cd3bc3547356d7108a134e46529e7a70f4b3
dahai78 <626071473@qq.com>
2015-11-06 16:19:09 +0800

    2015-11-5

---
+++ chapter2/ch5/1818红与黑.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+char ditan[20][20];
+int W,H:
+int X,Y;
+  
+struct Point{
+	int x;
+	int y;
+	
+};          
+
+void read(){
+	
+	queue<Point> Q;
+	  
+    for(int i=0 ;i<H; i++)
+       
+       for(int j=0; j<W; j++)
+          
+          {
+            cin >> ditan[i][j];
+            if( ditan[i][j] =='@')
+               {
+               	 X=i; Y=j;
+               }
+          }
+          
+}
+
+void work(int x,int y){
+	
+	Point p;
+	p.x=x;  p.y=y;
+	q.
+	
+}
+ 
+
+int main(int argc, char** argv) {
+	
+	cin >> W >> H;
+	
+	while( !(W==0 && j==0))
+	{
+		work(X, Y);
+		
+		
+		cin >> W >> H;
+	}
+	
+          
+          
+	
+	return 0;
+}
