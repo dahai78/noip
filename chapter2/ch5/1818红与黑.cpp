c4b9fb8a9ff161158eec12cade46c64e1323b46a
dahai78 <626071473@qq.com>
2015-11-07 21:43:45 +0800

    2011-11-7

--- chapter2/ch5/1818红与黑.cpp
+++ chapter2/ch5/1818红与黑.cpp
@@ -7,13 +7,15 @@ int dx[]={-1,1,0,0};
 int dy[]={0,0,-1,1};
 struct Point{
 	int x;
-	int y;		
-};          
+	int y;
+};
 
 int W,H;
+
+
 int work(int m,int n){
-	
-	int answer=0;	
+
+	int answer=0;
 	queue<Point> Q;
 	Point p ={m,n};
     Q.push(p);
@@ -27,45 +29,47 @@ int work(int m,int n){
 		{
 		    int newx = t.x + dx[i];
 		    int newy =t.y + dy[i];
-		    	
-			if	( newx >=0 && newx < W  && newy>=0 && newy <H && ditan[ newx ][ newy] == '.'  ) 
+
+			if	( newx >=0 && newx < H  && newy>=0 && newy <W && ditan[ newx ][ newy] == '.'  )
 			{
 				Point p = {newx,newy};
 				Q.push(p);
 				ditan[newx][newy] ='#';
-		    }			
+		    }
 		}
-		
+
 	}
-	
+
 	return answer;
-	
+
 }
- 
+
 
 int main(int argc, char** argv) {
-	
-    int X,Y;
-            
+
+    int startX,startY;
+
 	while( true)
 	{
 	   cin >> W >> H;
 	   if( W==0 && H==0) break;
-	   
+
 	   for(int i=0; i<H; i++)
         for(int j=0; j<W; j++)
          {
             cin >> ditan[i][j];
             if( ditan[i][j] =='@')
                {
-               	 X=i; Y=j;
+               	 startX=i; startY=j;
                }
-          }     
-		   
-		cout << work(X, Y) << endl;		
-	
-	}     
-	
+          }
+
+        //check();
+
+		cout << work(startX, startY) << endl;
+
+	}
+
 	return 0;
 }
 
